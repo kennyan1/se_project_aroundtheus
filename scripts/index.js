3ab2440b7436e5bf0e20e09872385c3a3ede48e5
kennyan1 <kennyanescobedo@gmail.com>
2024-06-13 21:42:50 -0700

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -23,3 +23,16 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
 }
 ];
+
+const profileEditButton = document.querySelector('#profile__edit-button');
+const profileEditModal = document.querySelector('#profile-edit-modal');
+
+profileEditButton.addEventListener('click', ()=>{
+    profileEditModal.classList.add('modal__open');
+
+});
+const profileCloseButton = document.querySelector ('#modal-profile-close');
+
+profileCloseButton.addEventListener('click', () => {
+    profileEditModal.classList.remove('modal__open');
+});
